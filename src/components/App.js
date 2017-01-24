5281f3092d647155982d2d9b15c939d9b9848ed8
madeleinehuish <vasistha@hotmail.com>
2017-01-24 11:19:23 -0800

    added react router to package.json and modified file tree for better organization

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+import { Link } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h2>Elan Vital Hello World Test</h2>
         </div>
         <p className="App-intro">
