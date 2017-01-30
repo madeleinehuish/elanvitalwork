eac1d50bfd7e186768cf78862b2073e7f6ff013b
Madeleine Huish <madeleinehuish@gmail.com>
2017-01-30 11:18:56 -0800

    basic dropdown setup working

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -15,7 +15,7 @@ class Header extends React.Component {
   }
 
   render() {
-    var user = 'joe';
+
     return (
       <header>
         <div className="twelve columns">
@@ -24,61 +24,58 @@ class Header extends React.Component {
           </div>
           <nav className="six columns">
             <ul>
-              <li>Give</li>
-              {/* <li>Projects</li> */}
+
               <Dropdown className="projects-dropdown" ref="dropdown">
-        <DropdownTrigger>
-          <span className="projects-dropdown__name">Projects</span>
-        </DropdownTrigger>
-        <DropdownContent>
-          {/* <div className="account-dropdown__identity account-dropdown__segment">
-            Signed in as <strong>{user.name}</strong>
-          </div> */}
-          <ul className="projects-dropdown__quick-links projects-dropdown__segment">
-            <li className="projects-dropdown__link">
-              <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                U.S.A.
-              </a>
-            </li>
-            <li className="projects-dropdown__link">
-              <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                Africa
-              </a>
-            </li>
-
-          </ul>
-        </DropdownContent>
-        </Dropdown>
-        <Dropdown>
-
-              {/* <li>About</li> */}
-              <DropdownTrigger>
-                <span className="account-dropdown__name">About</span>
-              </DropdownTrigger>
-              <DropdownContent>
-                {/* <div className="account-dropdown__identity account-dropdown__segment">
-                  Signed in as <strong>{user.name}</strong>
-                </div> */}
-                <ul className="account-dropdown__quick-links account-dropdown__segment">
-                  <li className="account-dropdown__link">
-                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                      Staff
-                    </a>
-                  </li>
-                  <li className="account-dropdown__link">
-                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                      Board of Directors
-                    </a>
-                  </li>
-                  <li className="account-dropdown__link">
-                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                      Advisory Board
-                    </a>
-                  </li>
-
-                </ul>
-              </DropdownContent>
+                <DropdownTrigger>
+                  <span className="projects-dropdown__name">Give</span>
+                </DropdownTrigger>
               </Dropdown>
+
+              <Dropdown className="projects-dropdown" ref="dropdown">
+                <DropdownTrigger>
+                  <span className="projects-dropdown__name">Projects</span>
+                </DropdownTrigger>
+                <DropdownContent>
+                  <ul className="projects-dropdown__quick-links projects-dropdown__segment">
+                    <li className="projects-dropdown__link">
+                      <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                        U.S.A.
+                      </a>
+                    </li>
+                    <li className="projects-dropdown__link">
+                      <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                        Africa
+                      </a>
+                    </li>
+                  </ul>
+                </DropdownContent>
+              </Dropdown>
+
+              <Dropdown>
+                <DropdownTrigger>
+                  <span className="account-dropdown__name">About</span>
+                </DropdownTrigger>
+                <DropdownContent>
+                  <ul className="account-dropdown__quick-links account-dropdown__segment">
+                    <li className="account-dropdown__link">
+                      <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                        Staff
+                      </a>
+                    </li>
+                    <li className="account-dropdown__link">
+                      <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                        Board of Directors
+                      </a>
+                    </li>
+                    <li className="account-dropdown__link">
+                      <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                        Advisory Board
+                      </a>
+                    </li>
+                  </ul>
+                </DropdownContent>
+              </Dropdown>
+
             </ul>
           </nav>
         </div>
