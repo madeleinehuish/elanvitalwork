c6e7bc05f4f350a936e70436c9cace42e0c730da
Madeleine Huish <madeleinehuish@gmail.com>
2017-01-30 10:48:07 -0800

    2 dropdowns

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,9 +1,6 @@
 import React from 'react';
 // import { Link } from 'react-router';
 import '../../styles/Header.css';
-
-// import Dropdown from 'react-simple-dropdown';
-
 import Dropdown, { DropdownContent, DropdownTrigger } from "react-simple-dropdown";
 
 class Header extends React.Component {
@@ -29,51 +26,59 @@ class Header extends React.Component {
             <ul>
               <li>Give</li>
               {/* <li>Projects</li> */}
-              <Dropdown className="account-dropdown" ref="dropdown">
+              <Dropdown className="projects-dropdown" ref="dropdown">
         <DropdownTrigger>
-          <span className="account-dropdown__name">Projects</span>
+          <span className="projects-dropdown__name">Projects</span>
         </DropdownTrigger>
         <DropdownContent>
           {/* <div className="account-dropdown__identity account-dropdown__segment">
             Signed in as <strong>{user.name}</strong>
           </div> */}
-          <ul className="account-dropdown__quick-links account-dropdown__segment">
-            <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+          <ul className="projects-dropdown__quick-links projects-dropdown__segment">
+            <li className="projects-dropdown__link">
+              <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
                 U.S.A.
               </a>
             </li>
-            <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+            <li className="projects-dropdown__link">
+              <a className="projects-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
                 Africa
               </a>
             </li>
-            {/* <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                Explore
-              </a>
-            </li>
-            <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                Help
-              </a>
-            </li>
-          </ul>
-          <ul className="account-dropdown__management-links account-dropdown__segment">
-            <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                Settings
-              </a>
-            </li>
-            <li className="account-dropdown__link">
-              <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
-                Sign out
-              </a>
-            </li> */}
+
           </ul>
         </DropdownContent>
-      </Dropdown>
-              <li>About</li>
+        </Dropdown>
+        <Dropdown>
+
+              {/* <li>About</li> */}
+              <DropdownTrigger>
+                <span className="account-dropdown__name">About</span>
+              </DropdownTrigger>
+              <DropdownContent>
+                {/* <div className="account-dropdown__identity account-dropdown__segment">
+                  Signed in as <strong>{user.name}</strong>
+                </div> */}
+                <ul className="account-dropdown__quick-links account-dropdown__segment">
+                  <li className="account-dropdown__link">
+                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                      Staff
+                    </a>
+                  </li>
+                  <li className="account-dropdown__link">
+                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                      Board of Directors
+                    </a>
+                  </li>
+                  <li className="account-dropdown__link">
+                    <a className="account-dropdown__link__anchor" href="#" onClick={this.handleLinkClick}>
+                      Advisory Board
+                    </a>
+                  </li>
+
+                </ul>
+              </DropdownContent>
+              </Dropdown>
             </ul>
           </nav>
         </div>
