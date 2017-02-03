2c8dd5b369baeb27ed74f6b151a8d9eebe8fe0e2
Madeleine Huish <madeleinehuish@gmail.com>
2017-02-02 18:06:37 -0800

    take 3, lol, as Mike says, this thing lints thoroughly...

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter, Match } from 'react-router';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 import Main from './Main';
