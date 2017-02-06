39da56f3547d1e06ed22216991fb4c3b1cee3ba8
Madeleine Huish <madeleinehuish@gmail.com>
2017-02-06 14:33:35 -0800

    take 2

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import MyPdfViewer from './MyPdfViewer';
+// import MyPdfViewer from './MyPdfViewer';
 import '../styles/Newsletter.css';
 
 
